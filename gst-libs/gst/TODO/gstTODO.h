35bbcb8400369700bcc3522b4fbe7932939d64da
Jafet Chaves <jafet.chaves@ridgerun.com>
2021-05-27 15:28:02 -0600

    Add GstTIOVX plugin template

---
+++ gst-libs/gst/TODO/gstTODO.h
@@ -0,0 +1,20 @@
+/*
+ Copyright (C) <TODO:YEAR> RidgeRun, LLC (http://www.ridgerun.com)
+ All Rights Reserved.
+ The contents of this software are proprietary and confidential to RidgeRun,
+ LLC.  No part of this program may be photocopied, reproduced or translated
+ into another programming language without prior written consent of
+ RidgeRun, LLC.  The user is free to modify the source code after obtaining
+ a software license from RidgeRun.  All source code changes must be provided
+ back to RidgeRun without any encumbrance.
+*/
+
+#ifndef __GST_TODO_H__
+#define __GST_TODO_H__
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+G_END_DECLS
+
+#endif //__GST_TODO_H__
