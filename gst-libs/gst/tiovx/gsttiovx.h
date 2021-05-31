997d9058e1513bb264c0a4c898347ceecb7c1c4a
Jafet Chaves <jafet.chaves@ridgerun.com>
2021-05-31 10:14:16 -0600

    Rename instance of "TODO" all around the project files

--- gst-libs/gst/tiovx/gsttiovx.h
+++ gst-libs/gst/tiovx/gsttiovx.h
@@ -1,5 +1,5 @@
 /*
- Copyright (C) <TODO:YEAR> RidgeRun, LLC (http://www.ridgerun.com)
+ Copyright (C) <2021> RidgeRun, LLC (http://www.ridgerun.com)
  All Rights Reserved.
  The contents of this software are proprietary and confidential to RidgeRun,
  LLC.  No part of this program may be photocopied, reproduced or translated
@@ -9,8 +9,12 @@
  back to RidgeRun without any encumbrance.
 */
 
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
+#ifndef __GST_TIOVX_H__
+#define __GST_TIOVX_H__
 
-#include "gstTODO.h"
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+G_END_DECLS
+
+#endif //__GST_TIOVX_H__
