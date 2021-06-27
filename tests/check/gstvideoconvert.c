9eef3f413d303058a6c5e581beef52964597c104
Daniel Chaves <danichg94@gmail.com>
2021-06-27 00:44:24 -0400

    Add the different resolution test

---
+++ tests/check/gstvideoconvert.c
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) [2021] Texas Instruments Incorporated
+ *
+ * All rights reserved not granted herein.
+ *
+ * Limited License.
+ *
+ * Texas Instruments Incorporated grants a world-wide, royalty-free,
+ * non-exclusive license under copyrights and patents it now or hereafter
+ * owns or controls to make, have made, use, import, offer to sell and sell
+ * ("Utilize") this software subject to the terms herein.  With respect to
+ * the foregoing patent license, such license is granted  solely to the extent
+ * that any such patent is necessary to Utilize the software alone.
+ * The patent license shall not apply to any combinations which include
+ * this software, other than combinations with devices manufactured by or
+ * for TI (“TI Devices”).  No hardware patent is licensed hereunder.
+ *
+ * Redistributions must preserve existing copyright notices and reproduce
+ * this license (including the above copyright notice and the disclaimer
+ * and (if applicable) source code license limitations below) in the
+ * documentation and/or other materials provided with the distribution
+ *
+ * Redistribution and use in binary form, without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * *    No reverse engineering, decompilation, or disassembly of this software
+ *      is permitted with respect to any software provided in binary form.
+ *
+ * *    Any redistribution and use are licensed by TI for use only with TI Devices.
+ *
+ * *    Nothing shall obligate TI to provide you with source code for the
+ *      software licensed and provided to you in object code.
+ *
+ * If software source code is provided to you, modification and redistribution
+ * of the source code are permitted provided that the following conditions are met:
+ *
+ * *    Any redistribution and use of the source code, including any resulting
+ *      derivative works, are licensed by TI for use only with TI Devices.
+ *
+ * *    Any redistribution and use of any object code compiled from the source
+ *      code and any resulting derivative works, are licensed by TI for use
+ *      only with TI Devices.
+ *
+ * Neither the name of Texas Instruments Incorporated nor the names of its
+ * suppliers may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * DISCLAIMER.
+ *
+ * THIS SOFTWARE IS PROVIDED BY TI AND TI’S LICENSORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL TI AND TI’S LICENSORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <gst/check/gstcheck.h>
+#include <gst/check/gstharness.h>
+#include <gst/gst.h>
+
+#include <gst-libs/gst/tiovx/gsttiovxsiso.h>
+
+GST_START_TEST (test_different_resolutions)
+{
+    GstElement * pipeline = NULL;
+    GstElement * nvstabilize = NULL;
+    GstStateChangeReturn ret = GST_STATE_CHANGE_SUCCESS;
+    GError * error = NULL;
+    GstCaps * current_caps = NULL;
+    GstPad * pad = NULL;
+    gchar nvstabilize_name[32] = "nvstabilize";
+
+    pipeline = gst_parse_launch (test_lines[TEST_BUILD_NVMM_PIPELINE], &error);
+    CHECK (TRUE == GST_IS_PIPELINE (pipeline) );
+
+    ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+    nvstabilize = gst_bin_get_by_name (GST_BIN (pipeline), nvstabilize_name);
+    CHECK (NULL != (GST_IS_ELEMENT (nvstabilize) ) );
+
+    pad = gst_element_get_static_pad (nvstabilize, "src");
+    g_usleep (1000000);
+    current_caps = gst_pad_get_current_caps (pad);
+
+    CHECK_TEXT ("memory:NVMM",
+      gst_caps_features_to_string (gst_caps_get_features (current_caps, 0) ) );
+
+    ret = gst_element_set_state (pipeline, GST_STATE_NULL);
+    CHECK (GST_STATE_CHANGE_FAILURE != ret);
+
+    gst_object_unref (pipeline);
+}
+
+GST_END_TEST;
+
+static Suite *
+gst_tiovx_siso_mock_suite (void)
+{
+  Suite *suite = suite_create ("videoconvert_caps");
+  TCase *tc = tcase_create ("general");
+
+  suite_add_tcase (suite, tc);
+  tcase_add_test (tc, test_different_resolutions);
+
+  return suite;
+}
+
+GST_CHECK_MAIN (test_different_resolutions);
