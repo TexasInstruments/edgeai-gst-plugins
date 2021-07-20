e7d4539795d089e7a4c58fa8d3472be26a502aeb
Jafet Chaves <jafet.chaves@ridgerun.com>
2021-07-20 17:31:13 -0600

    Rename variables to reflect that is a actually list of pointers

--- gst-libs/gst/tiovx/gsttiovxsimo.h
+++ gst-libs/gst/tiovx/gsttiovxsimo.h
@@ -108,7 +108,7 @@ struct _GstTIOVXSimoClass
 
   gboolean      (*create_graph)             (GstTIOVXSimo *self, vx_context context, vx_graph graph);
 
-  gboolean      (*get_node_info)            (GstTIOVXSimo *self, vx_reference ** input, vx_reference ** output,
+  gboolean      (*get_node_info)            (GstTIOVXSimo *self, vx_reference * input, vx_reference ** output,
 					                         vx_node * node, guint num_parameters);
 
   gboolean      (*configure_module)         (GstTIOVXSimo *self);
