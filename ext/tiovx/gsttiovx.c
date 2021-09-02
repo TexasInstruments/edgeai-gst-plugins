46796fd5564411852c7ef6cb907d7926b9ba4d1c
Emmanuel Madrigal <emmanuel.madrigal@ridgerun.com>
2021-09-02 11:08:19 -0600

    Fix error message type when registering tiovxmosaic

--- ext/tiovx/gsttiovx.c
+++ ext/tiovx/gsttiovx.c
@@ -111,7 +111,7 @@ ti_ovx_init (GstPlugin * plugin)
   ret = gst_element_register (plugin, "tiovxmosaic", GST_RANK_NONE,
       GST_TYPE_GST_TIOVX_MOSAIC);
   if (!ret) {
-    GST_ERROR ("Failed to register the tiovx-mosaic element");
+    GST_ERROR ("Failed to register the tiovxmosaic element");
     goto out;
   }
 
