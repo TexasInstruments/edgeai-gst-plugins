538f8f4c126c1c08c246bddaf1c1a856d3b3bc3d
Michael Gruner <michael.gruner@ridgerun.com>
2021-09-17 09:55:23 -0600

    Refactor test script to build utils only once

--- tests/check/test_utils.c
+++ tests/check/test_utils.c
@@ -59,21 +59,92 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef __TEST_UTILS_H__
-#define __TEST_UTILS_H__
+#include "test_utils.h"
 
-#include <gst/check/gstcheck.h>
+#define NUMBER_OF_STATE_CHANGES 5
 
-G_BEGIN_DECLS
+static void test_states_change_base (const gchar * pipe_desc,
+    GstStateChangeReturn * state_change);
 
-GstElement *test_create_pipeline (const gchar * pipe_desc);
-GstElement *test_create_pipeline_fail (const gchar * pipe_desc);
-void test_states_change (const gchar * pipe_desc);
-void test_fail_properties_configuration (const gchar * pipe_desc);
-void test_states_change_success (const gchar * pipe_desc);
+GstElement *
+test_create_pipeline (const gchar * pipe_desc)
+{
+  GstElement *pipeline = NULL;
+  GError *error = NULL;
 
-void test_states_change_fail (const gchar * pipe_desc);
+  GST_INFO ("testing pipeline %s", pipe_desc);
 
-G_END_DECLS
+  pipeline = gst_parse_launch (pipe_desc, &error);
 
-#endif
+  /* Check for errors creating pipeline */
+  fail_if (error != NULL, error);
+  fail_if (pipeline == NULL, error);
+
+  return pipeline;
+}
+
+GstElement *
+test_create_pipeline_fail (const gchar * pipe_desc)
+{
+  GstElement *pipeline = NULL;
+  GError *error = NULL;
+
+  GST_INFO ("testing pipeline %s", pipe_desc);
+
+  pipeline = gst_parse_launch (pipe_desc, &error);
+
+  /* Check for errors creating pipeline */
+  fail_if (error == NULL, error);
+
+  return pipeline;
+}
+
+static void
+test_states_change_base (const gchar * pipe_desc,
+    GstStateChangeReturn * state_change)
+{
+  GstElement *pipeline = NULL;
+  gint i = 0;
+
+  pipeline = test_create_pipeline (pipe_desc);
+
+  for (i = 0; i < NUMBER_OF_STATE_CHANGES; i++) {
+    fail_unless_equals_int (gst_element_set_state (pipeline, GST_STATE_PLAYING),
+        state_change[0]);
+    fail_unless_equals_int (gst_element_get_state (pipeline, NULL, NULL,
+            GST_CLOCK_TIME_NONE), state_change[1]);
+    fail_unless_equals_int (gst_element_set_state (pipeline, GST_STATE_NULL),
+        state_change[2]);
+  }
+  gst_object_unref (pipeline);
+}
+
+void
+test_states_change (const gchar * pipe_desc)
+{
+  GstStateChangeReturn state_change[] =
+      { GST_STATE_CHANGE_ASYNC, GST_STATE_CHANGE_SUCCESS,
+        GST_STATE_CHANGE_SUCCESS };
+
+  test_states_change_base (pipe_desc, state_change);
+}
+
+void
+test_states_change_fail (const gchar * pipe_desc)
+{
+  GstStateChangeReturn state_change[] =
+      { GST_STATE_CHANGE_FAILURE, GST_STATE_CHANGE_FAILURE,
+        GST_STATE_CHANGE_FAILURE };
+
+  test_states_change_base (pipe_desc, state_change);
+}
+
+void
+test_states_change_success (const gchar * pipe_desc)
+{
+  GstStateChangeReturn state_change[] =
+      { GST_STATE_CHANGE_SUCCESS, GST_STATE_CHANGE_SUCCESS,
+        GST_STATE_CHANGE_SUCCESS };
+
+  test_states_change_base (pipe_desc, state_change);
+}
