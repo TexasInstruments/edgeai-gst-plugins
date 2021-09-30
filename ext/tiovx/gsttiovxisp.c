3904e5013143497ec0bcd79c84c3b92097d778ca
Jafet Chaves <jafet.chaves@ridgerun.com>
2021-09-30 12:33:56 -0600

    Fix backlash alignment and misc spacing aspects

--- ext/tiovx/gsttiovxisp.c
+++ ext/tiovx/gsttiovxisp.c
@@ -1,61 +1,61 @@
 /*
  * Copyright (c) [2021] Texas Instruments Incorporated
- * 
+ *
  * All rights reserved not granted herein.
- * 
- * Limited License.  
- * 
- * Texas Instruments Incorporated grants a world-wide, royalty-free, 
- * non-exclusive license under copyrights and patents it now or hereafter 
- * owns or controls to make, have made, use, import, offer to sell and sell 
- * ("Utilize") this software subject to the terms herein.  With respect to 
+ *
+ * Limited License.
+ *
+ * Texas Instruments Incorporated grants a world-wide, royalty-free,
+ * non-exclusive license under copyrights and patents it now or hereafter
+ * owns or controls to make, have made, use, import, offer to sell and sell
+ * ("Utilize") this software subject to the terms herein.  With respect to
  * the foregoing patent license, such license is granted  solely to the extent
- * that any such patent is necessary to Utilize the software alone. 
- * The patent license shall not apply to any combinations which include 
+ * that any such patent is necessary to Utilize the software alone.
+ * The patent license shall not apply to any combinations which include
  * this software, other than combinations with devices manufactured by or
  * for TI (“TI Devices”).  No hardware patent is licensed hereunder.
- * 
- * Redistributions must preserve existing copyright notices and reproduce 
- * this license (including the above copyright notice and the disclaimer 
- * and (if applicable) source code license limitations below) in the 
+ *
+ * Redistributions must preserve existing copyright notices and reproduce
+ * this license (including the above copyright notice and the disclaimer
+ * and (if applicable) source code license limitations below) in the
  * documentation and/or other materials provided with the distribution
- * 
- * Redistribution and use in binary form, without modification, are permitted 
+ *
+ * Redistribution and use in binary form, without modification, are permitted
  * provided that the following conditions are met:
- * 
- * *	No reverse engineering, decompilation, or disassembly of this software 
+ *
+ * *    No reverse engineering, decompilation, or disassembly of this software
  *      is permitted with respect to any software provided in binary form.
- * 
- * *	Any redistribution and use are licensed by TI for use only with TI Devices.
- * 
- * *	Nothing shall obligate TI to provide you with source code for the
+ *
+ * *    Any redistribution and use are licensed by TI for use only with TI Devices.
+ *
+ * *    Nothing shall obligate TI to provide you with source code for the
  *      software licensed and provided to you in object code.
- * 
+ *
  * If software source code is provided to you, modification and redistribution
  * of the source code are permitted provided that the following conditions are met:
- * 
- * *	Any redistribution and use of the source code, including any resulting 
+ *
+ * *    Any redistribution and use of the source code, including any resulting
  *      derivative works, are licensed by TI for use only with TI Devices.
- * 
- * *	Any redistribution and use of any object code compiled from the source
- *      code and any resulting derivative works, are licensed by TI for use 
+ *
+ * *    Any redistribution and use of any object code compiled from the source
+ *      code and any resulting derivative works, are licensed by TI for use
  *      only with TI Devices.
- * 
- * Neither the name of Texas Instruments Incorporated nor the names of its 
- * suppliers may be used to endorse or promote products derived from this 
+ *
+ * Neither the name of Texas Instruments Incorporated nor the names of its
+ * suppliers may be used to endorse or promote products derived from this
  * software without specific prior written permission.
- * 
+ *
  * DISCLAIMER.
- * 
- * THIS SOFTWARE IS PROVIDED BY TI AND TI’S LICENSORS "AS IS" AND ANY EXPRESS 
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL TI AND TI’S LICENSORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ *
+ * THIS SOFTWARE IS PROVIDED BY TI AND TI’S LICENSORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL TI AND TI’S LICENSORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -86,19 +86,19 @@ enum
 #define TIOVX_ISP_SUPPORTED_HEIGHT "[1 , 8192]"
 
 /* Src caps */
-#define TIOVX_ISP_STATIC_CAPS_SRC \
-  "video/x-raw, "							\
-  "format = (string) " TIOVX_ISP_SUPPORTED_FORMATS_SRC ", "					\
-  "width = " TIOVX_ISP_SUPPORTED_WIDTH ", "					\
-  "height = " TIOVX_ISP_SUPPORTED_HEIGHT ", "					\
+#define TIOVX_ISP_STATIC_CAPS_SRC                            \
+  "video/x-raw, "                                            \
+  "format = (string) " TIOVX_ISP_SUPPORTED_FORMATS_SRC ", "  \
+  "width = " TIOVX_ISP_SUPPORTED_WIDTH ", "                  \
+  "height = " TIOVX_ISP_SUPPORTED_HEIGHT ", "                \
   "framerate = " GST_VIDEO_FPS_RANGE
 
 /* Sink caps */
-#define TIOVX_ISP_STATIC_CAPS_SINK \
-  "video/x-bayer, "							\
-  "format = (string) " TIOVX_ISP_SUPPORTED_FORMATS_SINK ", "					\
-  "width = " TIOVX_ISP_SUPPORTED_WIDTH ", "					\
-  "height = " TIOVX_ISP_SUPPORTED_HEIGHT ", "					\
+#define TIOVX_ISP_STATIC_CAPS_SINK                           \
+  "video/x-bayer, "                                          \
+  "format = (string) " TIOVX_ISP_SUPPORTED_FORMATS_SINK ", " \
+  "width = " TIOVX_ISP_SUPPORTED_WIDTH ", "                  \
+  "height = " TIOVX_ISP_SUPPORTED_HEIGHT ", "                \
   "framerate = " GST_VIDEO_FPS_RANGE
 
 /* Pads definitions */
@@ -186,12 +186,12 @@ gst_tiovx_isp_class_init (GstTIOVXISPClass * klass)
 
   src_temp =
       gst_pad_template_new_from_static_pad_template_with_gtype (&src_template,
-      GST_TIOVX_TYPE_PAD);
+      GST_TYPE_TIOVX_PAD);
   gst_element_class_add_pad_template (gstelement_class, src_temp);
 
   sink_temp =
       gst_pad_template_new_from_static_pad_template_with_gtype (&sink_template,
-      GST_TIOVX_TYPE_PAD);
+      GST_TYPE_TIOVX_PAD);
   gst_element_class_add_pad_template (gstelement_class, sink_temp);
 
   gobject_class->set_property = gst_tiovx_isp_set_property;
