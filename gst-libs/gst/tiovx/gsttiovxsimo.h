76bad43c16fbc36eac35c1cbe7680f7745e877b2
Jafet Chaves <jafet.chaves@ridgerun.com>
2021-10-05 11:14:11 -0600

    Add missing documentation of compare_caps function in SIMO class

--- gst-libs/gst/tiovx/gsttiovxsimo.h
+++ gst-libs/gst/tiovx/gsttiovxsimo.h
@@ -104,6 +104,9 @@ G_DECLARE_DERIVABLE_TYPE (GstTIOVXSimo, gst_tiovx_simo, GST,
  *                      implementation of caps events. Default
  *                      implementation is to use gst_caps_fixate() to obtain
  *                      caps that will be used in the src pads.
+ * @compare_caps:       Optional. Used in renegotiation cases. Subclasses can
+ *                      use this interface to implement a custom function that
+ *                      compares former sink caps and current sink caps.
  *
  * Subclasses can override any of the available virtual methods.
  */
