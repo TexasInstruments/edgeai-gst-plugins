42c30e27b87bcbb76ba00459249555a5599e8937
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-10-06 10:49:15 -0600

    Add the tiovxmosaic resolution test for startx/starty video offset on background

--- tests/check/gsttiovxmosaic.c
+++ tests/check/gsttiovxmosaic.c
@@ -349,6 +349,50 @@ GST_START_TEST (test_resolutions_larger_input_into_background)
 
 GST_END_TEST;
 
+GST_START_TEST (test_resolutions_random_startx_starty)
+{
+  TIOVXMosaicModeled element = { 0 };
+  g_autoptr (GString) pipeline = g_string_new ("");
+  g_autoptr (GString) upstream_caps = g_string_new ("");
+  g_autoptr (GString) downstream_caps = g_string_new ("");
+  g_autoptr (GString) properties = g_string_new ("");
+  gint32 width = 0;
+  gint32 height = 0;
+  gint32 startx = 0;
+  gint32 starty = 0;
+
+  gst_tiovx_mosaic_modeling_init (&element);
+
+  width =
+      g_random_int_range (element.sink_pad.width[0], element.sink_pad.width[1]);
+  height =
+      g_random_int_range (element.sink_pad.height[0],
+      element.sink_pad.height[1]);
+
+  startx = g_random_int_range (0, width);
+  starty = g_random_int_range (0, height);
+
+  /* Upstream caps */
+  g_string_printf (upstream_caps, "video/x-raw,width=%d,height=%d", width,
+      height);
+
+  /* Properties */
+  g_string_printf (properties, "sink_0::startx=%d sink_0::starty=%d", startx,
+      starty);
+
+  /* Downstream caps */
+  g_string_printf (downstream_caps, "video/x-raw,width=%d,height=%d", width,
+      height);
+
+  g_string_printf (pipeline,
+      "videotestsrc ! %s ! tiovxmosaic %s ! %s ! fakesink ", upstream_caps->str,
+      properties->str, downstream_caps->str);
+
+  test_states_change (pipeline->str);
+}
+
+GST_END_TEST;
+
 GST_START_TEST (test_framerate)
 {
   TIOVXMosaicModeled element = { 0 };
@@ -440,6 +484,7 @@ gst_state_suite (void)
   tcase_add_test (tc, test_resolutions_with_downscale_fail);
   tcase_add_test (tc, test_resolutions_smaller_input_into_background);
   tcase_add_test (tc, test_resolutions_larger_input_into_background);
+  tcase_add_test (tc, test_resolutions_random_startx_starty);
   tcase_add_test (tc, test_framerate);
   tcase_add_test (tc, test_request_random_number_of_pads);
   tcase_add_test (tc, test_property_target);
