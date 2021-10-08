bf9c57d021742bbd91626bb037168d64f3164697
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-10-07 19:03:07 -0600

    Add async non success state change

--- tests/check/test_utils.h
+++ tests/check/test_utils.h
@@ -79,6 +79,7 @@ GstElement *test_create_pipeline_fail (const gchar * pipe_desc);
 void test_fail_properties_configuration (const gchar * pipe_desc);
 void test_states_change_async (const gchar * pipe_desc,
     guint num_state_changes);
+void test_states_change_async_fail (const gchar * pipe_desc, guint num_state_changes);
 void test_states_change_fail (const gchar * pipe_desc, guint num_state_changes);
 void test_states_change_success (const gchar * pipe_desc,
     guint num_state_changes);
@@ -148,6 +149,17 @@ test_states_change_async (const gchar * pipe_desc, guint num_state_changes)
   GstStateChangeReturn state_change[] =
       { GST_STATE_CHANGE_ASYNC, GST_STATE_CHANGE_SUCCESS,
     GST_STATE_CHANGE_SUCCESS
+  };
+
+  test_states_change_base (pipe_desc, state_change, num_state_changes);
+}
+
+void
+test_states_change_async_fail (const gchar * pipe_desc, guint num_state_changes)
+{
+  GstStateChangeReturn state_change[] =
+      { GST_STATE_CHANGE_ASYNC, GST_STATE_CHANGE_FAILURE,
+    GST_STATE_CHANGE_FAILURE
   };
 
   test_states_change_base (pipe_desc, state_change, num_state_changes);
