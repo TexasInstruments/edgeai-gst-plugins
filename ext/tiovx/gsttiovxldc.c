0867d794a89a60259a50a817836522add0767442
Jafet Chaves <jafet.chaves@ridgerun.com>
2021-10-15 14:16:14 -0600

    Fix logic of gst_tiovx_ldc_get_sink_caps function

--- ext/tiovx/gsttiovxldc.c
+++ ext/tiovx/gsttiovxldc.c
@@ -1,61 +1,61 @@
 /*
  * Copyright (c) [2021] Texas Instruments Incorporated
- * 
+ *
  * All rights reserved not granted herein.
- * 
- * Limited License.  
- * 
- * Texas Instruments Incorporated grants a world-wide, royalty-free, 
- * non-exclusive license under copyrights and patents it now or hereafter 
- * owns or controls to make, have made, use, import, offer to sell and sell 
- * ("Utilize") this software subject to the terms herein.  With respect to 
+ *
+ * Limited License.
+ *
+ * Texas Instruments Incorporated grants a world-wide, royalty-free,
+ * non-exclusive license under copyrights and patents it now or hereafter
+ * owns or controls to make, have made, use, import, offer to sell and sell
+ * ("Utilize") this software subject to the terms herein.  With respect to
  * the foregoing patent license, such license is granted  solely to the extent
- * that any such patent is necessary to Utilize the software alone. 
- * The patent license shall not apply to any combinations which include 
+ * that any such patent is necessary to Utilize the software alone.
+ * The patent license shall not apply to any combinations which include
  * this software, other than combinations with devices manufactured by or
  * for TI (“TI Devices”).  No hardware patent is licensed hereunder.
- * 
- * Redistributions must preserve existing copyright notices and reproduce 
- * this license (including the above copyright notice and the disclaimer 
- * and (if applicable) source code license limitations below) in the 
+ *
+ * Redistributions must preserve existing copyright notices and reproduce
+ * this license (including the above copyright notice and the disclaimer
+ * and (if applicable) source code license limitations below) in the
  * documentation and/or other materials provided with the distribution
- * 
- * Redistribution and use in binary form, without modification, are permitted 
+ *
+ * Redistribution and use in binary form, without modification, are permitted
  * provided that the following conditions are met:
- * 
- * *	No reverse engineering, decompilation, or disassembly of this software 
+ *
+ * *	No reverse engineering, decompilation, or disassembly of this software
  *      is permitted with respect to any software provided in binary form.
- * 
+ *
  * *	Any redistribution and use are licensed by TI for use only with TI Devices.
- * 
+ *
  * *	Nothing shall obligate TI to provide you with source code for the
  *      software licensed and provided to you in object code.
- * 
+ *
  * If software source code is provided to you, modification and redistribution
  * of the source code are permitted provided that the following conditions are met:
- * 
- * *	Any redistribution and use of the source code, including any resulting 
+ *
+ * *	Any redistribution and use of the source code, including any resulting
  *      derivative works, are licensed by TI for use only with TI Devices.
- * 
+ *
  * *	Any redistribution and use of any object code compiled from the source
- *      code and any resulting derivative works, are licensed by TI for use 
+ *      code and any resulting derivative works, are licensed by TI for use
  *      only with TI Devices.
- * 
- * Neither the name of Texas Instruments Incorporated nor the names of its 
- * suppliers may be used to endorse or promote products derived from this 
+ *
+ * Neither the name of Texas Instruments Incorporated nor the names of its
+ * suppliers may be used to endorse or promote products derived from this
  * software without specific prior written permission.
- * 
+ *
  * DISCLAIMER.
- * 
- * THIS SOFTWARE IS PROVIDED BY TI AND TI’S LICENSORS "AS IS" AND ANY EXPRESS 
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL TI AND TI’S LICENSORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ *
+ * THIS SOFTWARE IS PROVIDED BY TI AND TI’S LICENSORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL TI AND TI’S LICENSORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -154,8 +154,7 @@ GST_DEBUG_CATEGORY_STATIC (gst_tiovx_ldc_debug);
 #define gst_tiovx_ldc_parent_class parent_class
 G_DEFINE_TYPE_WITH_CODE (GstTIOVXLDC, gst_tiovx_ldc,
     GST_TYPE_TIOVX_SIMO, GST_DEBUG_CATEGORY_INIT (gst_tiovx_ldc_debug,
-        "tiovxldc", 0, "debug category for the tiovxldc element");
-    );
+        "tiovxldc", 0, "debug category for the tiovxldc element"););
 
 static void
 gst_tiovx_ldc_set_property (GObject * object, guint prop_id,
@@ -738,6 +737,7 @@ gst_tiovx_ldc_get_sink_caps (GstTIOVXSimo * simo,
   GstCaps *sink_caps = NULL;
   GstCaps *template_caps = NULL;
   GList *l = NULL;
+  gint i = 0;
 
   g_return_val_if_fail (simo, NULL);
   g_return_val_if_fail (src_caps_list, NULL);
@@ -754,24 +754,24 @@ gst_tiovx_ldc_get_sink_caps (GstTIOVXSimo * simo,
   }
   gst_caps_unref (template_caps);
 
-  for (l = src_caps_list; l != NULL; l = l->next) {
-    GstCaps *src_caps = gst_caps_copy ((GstCaps *) l->data);
-    GstStructure *src_st = gst_caps_get_structure (src_caps, 0);
+  for (l = src_caps_list; l != NULL; l = g_list_next (l)) {
     GstCaps *tmp = NULL;
+    GstCaps *src_caps = gst_caps_copy ((GstCaps *) l->data);
 
-    /* Ignore resolution in intersection */
-    gst_structure_remove_fields (src_st, "width", "height", NULL);
-
-    if (gst_caps_can_intersect (sink_caps, src_caps)) {
-      tmp = gst_caps_intersect (sink_caps, src_caps);
-      gst_caps_unref (sink_caps);
-      gst_caps_unref (src_caps);
-      sink_caps = tmp;
-    } else {
-      gst_caps_unref (src_caps);
-      break;
+    for (i = 0; i < gst_caps_get_size (sink_caps); i++) {
+      GstStructure *src_st = gst_caps_get_structure (src_caps, i);
+      gst_structure_remove_fields (src_st, "width", "height", NULL);
     }
+
+    tmp = gst_caps_intersect (sink_caps, src_caps);
+    gst_caps_unref (sink_caps);
+    gst_caps_unref (src_caps);
+    sink_caps = tmp;
   }
+
+  GST_INFO_OBJECT (simo,
+      "Resulting supported sink caps by TIOVX LDC node: %"
+      GST_PTR_FORMAT, sink_caps);
 
   return sink_caps;
 }
